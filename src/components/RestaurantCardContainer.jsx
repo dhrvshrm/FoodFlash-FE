38b3b8b0fea6613749378b95c72eb7b4d4569479
Dhruv Sharma <shrmadhruv11@gmail.com>
2024-04-15 08:30:36 +0530

    feat: add router dom and contac & about pages

--- src/components/RestaurantCardContainer.jsx
+++ src/components/RestaurantCardContainer.jsx
@@ -44,8 +44,12 @@ export const RestaurantCardContainer = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.95250&lng=75.71050&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
+    console.log({
+      data: json.data.cards,
+    });
     const { restaurants } =
-      json.data.cards[3].card.card.gridElements?.infoWithStyle;
+      json.data.cards[3].card.card.gridElements?.infoWithStyle ||
+      json.data.cards[4].card.card.gridElements?.infoWithStyle;
     setIsLoading(false);
     setListOfRestaurant(restaurants);
   };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AboutPage from "./pages/About";
+import ContactPage from "./pages/Contact";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />,
+  },
+  {
+    path: "/cart",
+    element: <div>Cart</div>,
+  },
+]);
+
 root.render(
-  <React.StrictMode>
+  <RouterProvider router={router}>
     <App />
-  </React.StrictMode>
+  </RouterProvider>
 );
