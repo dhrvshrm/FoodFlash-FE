d896f5c3597cbcf0b5694a6d0dd92536d6345d17
Dhruv Sharma <shrmadhruv11@gmail.com>
2024-04-15 21:20:55 +0530

    fix: header and add routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-/* eslint-disable jsx-a11y/alt-text */
-const itmes = [
-  { name: "Home" },
-  { name: "Products" },
-  { name: "About Us" },
-  { name: "Cart" },
+import React from "react";
+import { Link } from "react-router-dom";
+
+const items = [
+  { name: "Home", path: "/" },
+  { name: "Products", path: "/products" },
+  { name: "About Us", path: "/about" },
+  { name: "Cart", path: "/cart" },
+  { name: "Contact", path: "/contact" },
 ];
 
 export const Header = () => {
@@ -13,25 +16,35 @@ export const Header = () => {
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
-        backgroundColor: " #c0d6e4",
-        border: "2px solid black",
+        backgroundColor: "#c0d6e4",
         margin: "0.5rem",
+        borderRadius: "1rem",
       }}
     >
       <img
         src="https://img.freepik.com/free-psd/persian-isolated-transparent-background_191095-39702.jpg?w=740&t=st=1711897038~exp=1711897638~hmac=d3eb10a98a4ebb52842cf0b1733879fae6d3ccb9e83b36995e381bb95dc63513s"
-        style={{ height: "5rem", width: "5rem", margin: "1rem 2rem" }}
+        style={{ height: "4em", width: "4rem", margin: "0.25rem 2rem" }}
+        alt="Logo"
       />
       <div
         style={{
           display: "flex",
           marginRight: "2rem",
           fontWeight: "700",
-          cursor: "pointer",
         }}
       >
-        {itmes.map((item) => (
-          <div style={{ margin: "0rem 1rem" }}>{item.name}</div>
+        {items.map((item, index) => (
+          <Link
+            key={index}
+            to={item.path}
+            style={{
+              margin: "0rem 1rem",
+              textDecoration: "none",
+              color: "black",
+            }}
+          >
+            {item.name}
+          </Link>
         ))}
       </div>
     </div>
