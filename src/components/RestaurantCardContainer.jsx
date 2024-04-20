206703e2f52e3d0476b72c9c7c7f1f8b2f399132
Dhruv Sharma <shrmadhruv11@gmail.com>
2024-04-20 17:37:19 +0530

    feat: add filter reset feature

--- src/components/RestaurantCardContainer.jsx
+++ src/components/RestaurantCardContainer.jsx
@@ -3,13 +3,14 @@ import { RestaurantCard } from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import FilterBtn from "./FilterBtn";
 import { Button, Input } from "@mui/material";
-import UserOffline from "./userOffline";
+import UserOffline from "./UserOffline";
 
 export const RestaurantCardContainer = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [searchText, setSearchText] = useState("");
   const [isOnline, setIsOnline] = useState(navigator.onLine);
+  const [originalListOfRestaurant, setOriginalListOfRestaurant] = useState([]);
 
   const updateOnlineStatus = () => {
     setIsOnline(navigator.onLine);
@@ -41,12 +42,16 @@ export const RestaurantCardContainer = () => {
 
   const imgUrl = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660`;
 
-  function handleFilterClick() {
-    const filteredList = listOfRestaurant.filter(
-      (restaurant) => restaurant.info.avgRating > 4.2
-    );
-    setListOfRestaurant(filteredList);
-  }
+  const handleFilterClick = (reset = false) => {
+    if (reset) {
+      setListOfRestaurant(originalListOfRestaurant);
+    } else {
+      const filteredList = originalListOfRestaurant.filter(
+        (restaurant) => restaurant.info.avgRating > 4.2
+      );
+      setListOfRestaurant(filteredList);
+    }
+  };
 
   useEffect(() => {
     if (searchText === "") {
@@ -60,101 +65,118 @@ export const RestaurantCardContainer = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.95250&lng=75.71050&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    console.log({
-      data: json.data.cards,
-    });
     const { restaurants } =
       json.data.cards[3].card.card.gridElements?.infoWithStyle ||
       json.data.cards[4].card.card.gridElements?.infoWithStyle;
     setIsLoading(false);
     setListOfRestaurant(restaurants);
-  };
+    setOriginalListOfRestaurant(restaurants);
 
-  return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          border: "2px solid black",
-          borderRadius: "1rem",
-          backgroundColor: " #c0d6e4",
-        }}
-      >
-        <FilterBtn handleFilterClick={handleFilterClick} />
-        <Input
-          placeholder="Search for your Favourite Dishes ... "
-          autoFocus={false}
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-          sx={{
-            height: "2rem",
-            margin: "1rem 2rem",
-            width: "20rem",
-            paddingLeft: "0.5rem",
-            fontFamily: "Raleway, sans-serif",
-            backgroundColor: "white",
-          }}
-        />
-        <Button
-          onClick={handleSearch}
-          sx={{
-            height: "2rem",
-            margin: "1rem 2rem",
-            width: "5rem",
-            borderRadius: "1rem",
-            backgroundColor: "lightGray",
-            cursor: "pointer",
-            color: "black",
-            border: "1px solid black",
-            textTransform: "none",
-            "&:hover": {
-              backgroundColor: " #c0d6e4",
-            },
-            fontWeight: 600,
-          }}
-        >
-          Search
-        </Button>
-      </div>
-      {!isOnline ? (
-        <UserOffline />
-      ) : (
+    return (
+      <>
         <div
           style={{
             display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            gap: "2rem",
+            alignItems: "center",
+            justifyContent: "space-between",
             border: "2px solid black",
-            padding: "2rem",
+            borderRadius: "1rem",
+            backgroundColor: " #c0d6e4",
           }}
         >
-          {isLoading ? (
-            <Shimmer count={12} />
-          ) : (
-            <>
-              {listOfRestaurant?.map((restaurant, index) => (
-                <RestaurantCard
-                  key={index}
-                  name={restaurant.info.name}
-                  cuisine={restaurant.info.cuisines.slice(0, 5).join(", ")}
-                  rating={`⭐ ${
-                    restaurant.info.avgRating
-                      ? restaurant.info.avgRating
-                      : "N/A"
-                  }`}
-                  deliveryTime={`🕒 ${restaurant.info.sla.deliveryTime} min`}
-                  imgUrl={`${imgUrl}/${restaurant.info.cloudinaryImageId}`}
-                  isLoading={isLoading}
-                  id={restaurant.info.id}
-                />
-              ))}
-            </>
-          )}
+          <FilterBtn handleFilterClick={handleFilterClick(true)} />
+          <Button
+            onClick={handleFilterClick}
+            sx={{
+              height: "2rem",
+              width: "5rem",
+              borderRadius: "1rem",
+              backgroundColor: "lightGray",
+              cursor: "pointer",
+              color: "black",
+              border: "1px solid black",
+              textTransform: "none",
+              "&:hover": {
+                backgroundColor: " #c0d6e4",
+              },
+              fontWeight: 600,
+            }}
+          >
+            Reset
+          </Button>
+          <Input
+            placeholder="Search for your Favourite Dishes ... "
+            autoFocus={false}
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            sx={{
+              height: "2rem",
+              margin: "1rem 2rem",
+              width: "20rem",
+              paddingLeft: "0.5rem",
+              fontFamily: "Raleway, sans-serif",
+              backgroundColor: "white",
+            }}
+          />
+          <Button
+            onClick={handleSearch}
+            sx={{
+              height: "2rem",
+              margin: "1rem 2rem",
+              width: "5rem",
+              borderRadius: "1rem",
+              backgroundColor: "lightGray",
+              cursor: "pointer",
+              color: "black",
+              border: "1px solid black",
+              textTransform: "none",
+              "&:hover": {
+                backgroundColor: " #c0d6e4",
+              },
+              fontWeight: 600,
+            }}
+          >
+            Search
+          </Button>
         </div>
-      )}
-    </>
-  );
+        {!isOnline ? (
+          <UserOffline />
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              gap: "2rem",
+              border: "2px solid black",
+              padding: "2rem",
+            }}
+          >
+            {isLoading ? (
+              <Shimmer count={12} />
+            ) : (
+              <>
+                {listOfRestaurant?.map((restaurant, index) => (
+                  <RestaurantCard
+                    key={index}
+                    name={restaurant.info.name}
+                    cuisine={restaurant.info.cuisines.slice(0, 5).join(", ")}
+                    rating={`⭐ ${
+                      restaurant.info.avgRating
+                        ? restaurant.info.avgRating
+                        : "N/A"
+                    }`}
+                    deliveryTime={`🕒 ${restaurant.info.sla.deliveryTime} min`}
+                    imgUrl={`${imgUrl}/${restaurant.info.cloudinaryImageId}`}
+                    isLoading={isLoading}
+                    id={restaurant.info.id}
+                  />
+                ))}
+              </>
+            )}
+          </div>
+        )}
+      </>
+    );
+  };
 };
