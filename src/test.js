d09d5fc254df04c540d38eb2808087a3487f509d
Dhruv Sharma <shrmadhruv11@gmail.com>
2024-05-18 01:23:06 +0530

    feat: add restaurant dumy data for dev

---
+++ src/test.js
@@ -0,0 +1,186 @@
+const test = {
+  id: "47595",
+  name: "Pizza Hut",
+  city: "Jaipur",
+  slugs: {
+    restaurant: "pizza-hut-c-scheme-c-scheme",
+    city: "jaipur",
+  },
+  uniqueId: "e51e6528-4859-48c8-8fed-e84c17dc2d76",
+  cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+  locality: "C Scheme",
+  areaName: "MI Road",
+  costForTwo: "35000",
+  costForTwoMessage: "₹350 for two",
+  cuisines: ["Pizzas"],
+  avgRating: 4.1,
+  feeDetails: {
+    restaurantId: "47595",
+    fees: [
+      {
+        name: "TYPE_DISTANCE",
+        fee: 8500,
+      },
+      {
+        name: "TYPE_TIME",
+      },
+    ],
+    totalFee: 8500,
+    title: "Delivery Charge",
+    amount: "8500",
+    icon: "v1648208530/surgecreatives/info",
+    message: "<b>Very Far (11.2 kms)</b> | Additional delivery fee will apply",
+  },
+  parentId: "721",
+  avgRatingString: "4.1",
+  totalRatingsString: "5K+ ratings",
+  sla: {
+    restaurantId: "47595",
+    deliveryTime: 47,
+    minDeliveryTime: 45,
+    maxDeliveryTime: 50,
+    lastMileTravel: 11.2,
+    serviceability: "SERVICEABLE",
+    stressFactor: 1,
+    rainMode: "RAIN_MODE_NONE",
+    longDistance: "LONG_DISTANCE_IT_IS_LONG_DISTANCE",
+    zoneId: 391,
+    slaString: "45-50 MINS",
+    lastMileTravelString: "11.1 km",
+    iconType: "ICON_TYPE_EMPTY",
+  },
+  availability: {
+    nextCloseTime: "2024-05-18 04:00:00",
+    visibility: true,
+    opened: true,
+    restaurantClosedMeta: {},
+  },
+  aggregatedDiscountInfo: {
+    shortDescriptionList: [
+      {
+        discountType: "Percentage",
+        operationType: "RESTAURANT",
+      },
+      {
+        discountType: "Percentage",
+        operationType: "RESTAURANT",
+      },
+    ],
+    descriptionList: [
+      {
+        meta: "20% off upto ₹125 | Use KOTAK811 Above ₹499",
+        discountType: "Percentage",
+        operationType: "RESTAURANT",
+      },
+      {
+        meta: "15% off upto ₹150 |  Use HSBCFEST Above ₹499",
+        discountType: "Percentage",
+        operationType: "RESTAURANT",
+      },
+    ],
+    visible: true,
+  },
+  badges: {},
+  slugString: "pizza-hut-c-scheme-c-scheme",
+  multiOutlet: true,
+  isOpen: true,
+  labels: [
+    {
+      title: "Timings",
+      message: "null",
+    },
+    {
+      title: "Address",
+      message: "109, Ganpati Plaza, MI Road, Jaipur",
+    },
+    {
+      title: "Cuisines",
+      message: "Pizzas",
+    },
+  ],
+  logo: "rng/md/carousel/production/cztbor82z4wcxssgnaje",
+  totalRatings: 5000,
+  aggregatedDiscountInfoV2: {
+    shortDescriptionList: [
+      {
+        discountType: "Percentage",
+        operationType: "RESTAURANT",
+      },
+      {
+        discountType: "Percentage",
+        operationType: "RESTAURANT",
+      },
+    ],
+    descriptionList: [
+      {
+        meta: "20% off upto ₹125 | Use KOTAK811 Above ₹499",
+        discountType: "Percentage",
+        operationType: "RESTAURANT",
+      },
+      {
+        meta: "15% off upto ₹150 |  Use HSBCFEST Above ₹499",
+        discountType: "Percentage",
+        operationType: "RESTAURANT",
+      },
+    ],
+    couponDetailsCta: "View coupon details",
+  },
+  type: "F",
+  headerBanner: {
+    url: "swiggy://webview?is_external=false&webview_url=https://www.swiggy.com/restaurant-info/47595",
+  },
+  expectationNotifiers: [
+    {
+      text: "<b>Very Far (11.2 kms)</b> | Additional delivery fee will apply",
+      icon: {
+        imageId: "equitable_serviceability/es-icons/es-location-icon.png",
+        clickable: true,
+      },
+      popup: {
+        popupType: "HALF_CARD",
+        cta: {},
+      },
+      type: "DISTANCE_FEE_FOOD_LM",
+      enrichedText:
+        "<b>Very Far (11.2 kms)</b> | Additional delivery fee will apply",
+      halfCardPopup: {
+        halfCardPopupHeader: {
+          message: "Ordering from your far away favourite!",
+          imageId: "equitable_serviceability/es-icons/es-half-card-icon.png",
+        },
+        halfCardPopupDescription: [
+          {
+            message:
+              "Far away orders generally take longer and may impact the food temperature",
+            imageId: "equitable_serviceability/es-icons/es-half-card-food.png",
+          },
+        ],
+      },
+      trailingIcon: {},
+    },
+  ],
+  generalPurposeInfoListV2: [
+    {
+      cta: {
+        info: {
+          recordings: {},
+        },
+        linkCta: {},
+      },
+    },
+  ],
+  ratingSlab: "RATING_SLAB_5",
+  orderabilityCommunication: {
+    title: {},
+    subTitle: {},
+    message: {},
+    customIcon: {},
+  },
+  hasBestsellerItems: true,
+  cartOrderabilityNudgeBanner: {
+    parameters: {},
+    presentation: {},
+  },
+  latLong: "26.91952283,75.7997265",
+  backgroundImageOverlayInfo: {},
+};
