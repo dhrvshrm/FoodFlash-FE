4278b0b5d1df1669c2e9ad091cfb53af781b65b9
Dhruv Sharma <shrmadhruv11@gmail.com>
2024-05-18 22:13:08 +0530

    refactor: restaurant card styles

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,6 +1,7 @@
 import { Box, Stack, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import React from "react";
+import { STYLES } from "./restaurantCard.styles";
 
 export const RestaurantCard = (props) => {
   const navigate = useNavigate();
@@ -13,87 +14,19 @@ export const RestaurantCard = (props) => {
   }
 
   return (
-    <Stack
-      sx={{
-        width: "18rem",
-        cursor: "pointer",
-        display: "flex",
-        flexDirection: "column",
-        padding: "1.5rem 1.5rem 1rem 1.5rem",
-        transition: "transform 0.3s ease-in-out",
-        mb: "1rem",
-        "&:hover": {
-          transform: "scale(1.02)",
-        },
-      }}
-      onClick={onCardClick}
-    >
-      <div style={{ position: "relative", height: "12rem", width: "17rem" }}>
-        <img
-          src={props.imgUrl}
-          style={{
-            objectFit: "cover",
-
-            borderRadius: "1rem",
-            alignSelf: "center",
-            width: "100%",
-            height: "100%",
-          }}
-          alt="Restaurant"
-        />
-        <Box
-          sx={{
-            position: "absolute",
-            bottom: 0,
-            left: 0,
-            width: "100%",
-            height: "8rem",
-            background:
-              "linear-gradient(to bottom, transparent, rgba(0, 0, 0, 2))",
-            borderRadius: "0 0 1rem 1rem",
-          }}
-        >
-          <Typography
-            sx={{
-              color: "white",
-              padding: "0.5rem",
-              fontWeight: 400,
-              bottom: 2,
-              left: 2,
-              mt: "4.5rem",
-              ml: "1rem",
-              fontFamily: "Poetsen One",
-              fontSize: "1.3rem",
-            }}
-          >
+    <Stack sx={STYLES.cardContainer} onClick={onCardClick}>
+      <div style={STYLES.imageContainer}>
+        <img src={props.imgUrl} style={STYLES.image} alt="Restaurant" />
+        <Box sx={STYLES.overlay}>
+          <Typography sx={STYLES.overlayText}>
             {header} {subHeader}
           </Typography>
         </Box>
       </div>
-      <Box
-        sx={{
-          marginTop: "0.5rem",
-          marginLeft: "1rem",
-        }}
-      >
-        <Typography
-          sx={{
-            fontWeight: 700,
-            marginBottom: "0.5rem",
-            textOverflow: "ellipsis",
-            overflow: "hidden",
-            whiteSpace: "nowrap",
-            fontFamily: "Poetsen One",
-            fontSize: "1.2rem",
-          }}
-        >
-          {props.name}
-        </Typography>
+      <Box sx={STYLES.detailsContainer}>
+        <Typography sx={STYLES.restaurantName}>{props.name}</Typography>
         {propsArr.slice(1, 4).map((item) => (
-          <Typography
-            sx={{ fontWeight: 500, fontFamily: "Poetsen One" }}
-            key={item}
-          >
+          <Typography sx={STYLES.additionalInfo} key={item}>
             {item}
           </Typography>
         ))}
