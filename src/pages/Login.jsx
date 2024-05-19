16decf57c8bbbcaa1ba5ac5c7873c20aa37e18d5
Dhruv Sharma <shrmadhruv11@gmail.com>
2024-05-19 14:16:53 +0530

    feat: login page carousel

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,13 @@
-import { Box, Grid, Paper, Stack } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import { Box, Stack } from "@mui/material";
+import React from "react";
 import LoginForm from "../components/LoginForm";
-import Carousel2 from "../assets/Carousel1.png";
-import Carousel1 from "../assets/Carousel2.png";
-import Carousel3 from "../assets/Carousel3.png";
+import { Carousel } from "../components/Carousel";
 
 const styles = {
   root: {
     position: "relative",
     overflow: "hidden",
-    width: "100%",
+    // width: "100%",
     height: "100%",
   },
 
@@ -57,74 +55,16 @@ const styles = {
   },
 };
 
-const STYLES = {
-  images: [Carousel1, Carousel2, Carousel3],
-};
-
-const Carousel = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const handleDotClick = (index) => {
-    setCurrentSlide(index);
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide((currentSlide) =>
-        currentSlide === STYLES.images.length - 1 ? 0 : currentSlide + 1
-      );
-    }, 3000);
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <div style={styles.root}>
-      <Grid container spacing={0}>
-        <Grid item xs={12}>
-          <div
-            style={{
-              ...styles.carousel,
-              transform: `translateX(-${currentSlide * 100}%)`,
-            }}
-          >
-            {STYLES.images.map((image, index) => (
-              <Paper key={index} style={styles.slide}>
-                <img src={image} alt={`Slide ${index}`} style={styles.image} />
-              </Paper>
-            ))}
-          </div>
-        </Grid>
-        <Grid item xs={12}>
-          <div style={styles.dotsContainer}>
-            {STYLES.images.map((_, index) => (
-              <div
-                key={index}
-                style={{
-                  ...styles.dot,
-                  backgroundColor: currentSlide === index ? "#000" : "#ccc",
-                }}
-                onClick={() => handleDotClick(index)}
-              />
-            ))}
-          </div>
-        </Grid>
-      </Grid>
-    </div>
-  );
-};
-
 const Login = () => {
   return (
-    <Stack direction="row" sx={{ backgroundColor: "black" }}>
-      <Stack
-        direction="row"
-        sx={{ width: "75%", height: "100vh", outline: "none" }}
-      >
-        <Carousel />
-        <Box sx={styles.aboveBox}>
-          <LoginForm />
-        </Box>
-      </Stack>
+    <Stack
+      direction="row"
+      sx={{ width: "100%", height: "100vh", outline: "none" }}
+    >
+      <Carousel />
+      <Box sx={styles.aboveBox}>
+        <LoginForm />
+      </Box>
     </Stack>
   );
 };
